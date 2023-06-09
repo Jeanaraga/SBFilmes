c4402413e422e63c7abc79171f63da7ce60fe205
Jean Pedro <jeanjesuspedrobook@gmail.com>
2023-06-09 12:13:30 -0300

    update

--- Login-Trabalho-Final-main/assets/js/script.js
+++ Login-Trabalho-Final-main/assets/js/script.js
@@ -5,13 +5,35 @@ const submitButton = document.querySelector('button');
 // Adiciona um evento de envio ao formulário
 form.addEventListener('submit', function(event) {
   event.preventDefault(); // Impede o envio do formulário
+// Obtém os valores dos campos do formulário
+const name = document.querySelector('#name').value;
+const email = document.querySelector('#email').value;
+const password = document.querySelector('#password').value;
+const confirmPassword = document.querySelector('#senha2').value;
+const dateOfBirth = document.querySelector('#datebirth').value;
 
-  // Obtém os valores dos campos do formulário
-  const name = document.querySelector('#name').value;
-  const email = document.querySelector('#email').value;
-  const password = document.querySelector('#password').value;
-  const confirmPassword = document.querySelector('#senha2').value;
-  const dateOfBirth = document.querySelector('#datebirth').value;
+// Verifica se algum dos campos está vazio
+if (name === '' || email === '' || password === '' || dateOfBirth === '') {
+  // Exibe uma mensagem de erro caso algum campo esteja vazio
+  alert('Por favor, preencha todos os campos obrigatórios.');
+  return; // Encerra a função para impedir o envio do formulário
+}
+
+// Verifica o formato da data de nascimento
+const dateOfBirthRegex = /^\d{4}-\d{2}-\d{2}$/;
+if (!dateOfBirthRegex.test(dateOfBirth)) {
+  // Exibe uma mensagem de erro caso o formato da data de nascimento seja inválido
+  alert('Por favor, digite uma data de nascimento válida no formato AAAA-MM-DD.');
+  return; // Encerra a função para impedir o envio do formulário
+}
+
+// Verifica se a data de nascimento é maior que 2022 ou menor que 1900
+const birthYear = parseInt(dateOfBirth.substring(0, 4), 10);
+if (birthYear > 2022 || birthYear < 1900) {
+  // Exibe uma mensagem de erro caso a data de nascimento seja inválida
+  alert('Por favor, digite uma data de nascimento válida entre 1900 e 2022.');
+  return; // Encerra a função para impedir o envio do formulário
+}
 
   // Verifica se algum dos campos está vazio
   if (name === '' || email === '' || password === '') {
@@ -19,6 +41,16 @@ form.addEventListener('submit', function(event) {
     alert('Por favor, preencha todos os campos obrigatórios.');
     return; // Encerra a função para impedir o envio do formulário
   }
+
+  // Verifica se o e-mail possui o domínio ".com"
+if (!email.endsWith('.com')) {
+  // Exibe uma mensagem de erro caso o e-mail não tenha o domínio ".com"
+  const errorMessage = 'Digite um e-mail válido com o domínio ".com".';
+  // const errorContainer = document.querySelector('.error-container');
+ alert(errorMessage)
+  return; // Encerra a função para impedir o envio do formulário
+}
+
 
   // Verifica se as senhas correspondem
   if (password !== confirmPassword) {
